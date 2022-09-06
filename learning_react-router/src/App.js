e891bb824fd618aa67e51f38593391f59e89d3fd
Makotox0207 <xiaoxishicheng@gmail.com>
2022-09-07 00:33:31 +0900

    SPAで簡易的に画面遷移

--- learning_react-router/src/App.js
+++ learning_react-router/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/page1">1つ目のページに遷移</Link>
+      <Link to="/page2">2つ目のページに遷移</Link>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/page1"><Page1 /></Route>
+        <Route path="/page2"><Page2 /></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
