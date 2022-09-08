6c162e050655b98f69f94681740822ebe6367028
Makotox0207 <xiaoxishicheng@gmail.com>
2022-09-09 01:22:54 +0900

    linkでstateを渡す

--- learning_react-router/src/Page1.jsx
+++ learning_react-router/src/Page1.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 export const Page1 = () => {
+  const detailA= 'detailAです';
   return (
     <>
       <h1>Page1です</h1>
-      <Link to="page1/detailA">DetailA</Link>
-      <br />
+      <Link to={{pathname: "page1/detailA", state: detailA}}>DetailA</Link>
       <Link to="page1/detailB">DetailB</Link>
+      <br />
     </>
   )
 }
--- learning_react-router/src/Page1DetailB.jsx
+++ learning_react-router/src/Page1DetailB.jsx
@@ -1,4 +1,8 @@
+import { useLocation } from 'react-router-dom';
+
 export const Page1DetailB = () => {
+  const { state } = useLocation();
+  console.log(state);
   return (
     <h1>Page1DetailBです</h1> 
   )
