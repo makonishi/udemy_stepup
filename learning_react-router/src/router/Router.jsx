ed2e273bb0755a3d10041eb9ead585461faa1d91
Makotox0207 <xiaoxishicheng@gmail.com>
2022-09-08 23:11:23 +0900

    page1のネストされたルーティングを分割する

--- learning_react-router/src/router/Router.jsx
+++ learning_react-router/src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import { Home } from '../Home';
-import { Page1 } from '../Page1';
-import { Page1DetailA } from '../Page1DetailA';
-import { Page1DetailB } from '../Page1DetailB';
 import { Page2 } from '../Page2';
+import { page1Route } from './Page1Route';
 
 export const Router = () => {
   return (
@@ -11,9 +9,15 @@ export const Router = () => {
       <Route exact path="/"><Home /></Route>
       <Route path="/page1" render={({match: { url }}) => (
         <Switch>
-          <Route exact path={url}><Page1 /></Route>
-          <Route path={`${url}/detailA`}><Page1DetailA /></Route>
-          <Route path={`${url}/detailB`}><Page1DetailB /></Route>
+          {/* => {}ではなく=> ()を使うと, returnを省略できる. */}
+          {page1Route.map((route) => (
+            <Route
+              key={route.path}
+              exact={route.exact}
+              path={`${url}${route.path}`}>
+                {route.children}
+            </Route>
+          ))}
         </Switch>
       )} />
       <Route path="/page2"><Page2 /></Route>
