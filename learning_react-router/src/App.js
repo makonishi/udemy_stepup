03b99cb5f8c550ecb42dc83f269e670abeb34d61
Makotox0207 <xiaoxishicheng@gmail.com>
2022-09-08 22:41:01 +0900

    ルーティングの分割

--- learning_react-router/src/App.js
+++ learning_react-router/src/App.js
@@ -1,27 +1,16 @@
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page2 } from './Page2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { Router } from './router/Router';
 
 function App() {
   return (
     <BrowserRouter>
       <Link to="/">Home</Link>
+      <br />
       <Link to="/page1">1つ目のページに遷移</Link>
+      <br />
       <Link to="/page2">2つ目のページに遷移</Link>
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/page1" render={({match: { url }}) => (
-          <Switch>
-            <Route exact path={url}><Page1 /></Route>
-            <Route path={`${url}/detailA`}><Page1DetailA /></Route>
-            <Route path={`${url}/detailB`}><Page1DetailB /></Route>
-          </Switch>
-        )} />
-        <Route path="/page2"><Page2 /></Route>
-      </Switch>
+      <br />
+      <Router />
     </BrowserRouter>
   );
 }
