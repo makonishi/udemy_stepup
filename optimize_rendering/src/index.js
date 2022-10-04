a379d4ba37c0e1a8b070ca9f08d0996105ecb151
Makotox0207 <xiaoxishicheng@gmail.com>
2022-10-05 04:18:13 +0900

    コンポーネントのメモ化

--- optimize_rendering/src/index.js
+++ optimize_rendering/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
